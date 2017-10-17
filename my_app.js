8e563f992ba25d0769f7b1399d331badc4111aa0
nowos <31601190+nowos@users.noreply.github.com>
2017-10-17 11:48:12 +0200

    Add files via upload

---
+++ my_app.js
@@ -0,0 +1,24 @@
+let elements = 
+        document.getElementsByName('cssProperty');
+
+let div = document.getElementById('modify');
+
+var set = function()
+{  
+    for(let index = 0; index < elements.length; index++)
+{
+        let cssProperty = elements[ index ].getAttribute('id');
+
+        let cssValue = elements[ index ].value ;
+        
+        div.style[cssProperty] = cssValue; 
+}
+};
+document.getElementById('bd').addEventListener('change', set);
+ 
+
+
+
+
+
+
